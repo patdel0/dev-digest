96be07eec644ba201ca25f19353db50cef444c0d
patdel0 <zefiru@protonmail.com>
2024-03-22 00:29:30 +0000

    Create a Hello World API endpoint
    This is for the sake of demonstrating how API routes are created as well
    as creating an endpoint to test the integration between the frontend and
    backend.
    
    This creates an API endpoint found in http://127.0.0.1:8000/api/hello/.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+## Hello world API view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HelloWorldAPIView(APIView):
+    def get(self, request):
+        return Response(data={"message": "Hello World"}, status=status.HTTP_200_OK)
