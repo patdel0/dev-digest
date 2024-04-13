088db383f8edf233ebe345a6d1349f8e3248a845
patdel0 <zefiru@protonmail.com>
2024-04-13 01:51:21 +0100

    chore: Transform function into command
    This allows us to ssh into the backend server and trigger the job
    manually if necessary.

--- backend/api/management/commands/daily_update_articles_db.py
+++ backend/api/management/commands/daily_update_articles_db.py
@@ -1,11 +1,16 @@
+from django.core.management.base import BaseCommand
 from api.models import Article, Source
-from .curate_articles import curate_articles
+from api.management.commands.curate_articles import curate_articles
 
-def run_cron_job():
-    sources = Source.objects.values_list('feed_url', flat=True)
-    articles = curate_articles(sources)
+class Command(BaseCommand):
+    help = 'Updates articles database from sources'
 
-    for item in articles:
-        Article.objects.create(title=item['title'], content=item['content'].get_text())
+    def handle(self, *args, **options):
+        sources = Source.objects.values_list('feed_url', flat=True)
+        articles = curate_articles(sources)
 
-    return articles
+        for item in articles:
+            Article.objects.create(title=item['title'], content=item['content'].get_text())
+            self.stdout.write(self.style.SUCCESS('Article added'))
+
+        self.stdout.write(self.style.SUCCESS('Cron job complete'))
--- backend/api/tests/management/commands/test_daily_update_articles_db.py
+++ backend/api/tests/management/commands/test_daily_update_articles_db.py
@@ -1,14 +1,14 @@
 import pytest
 import requests
+from django.core.management import call_command
 from api.models import Article, Source
-from api.management.commands.daily_update_articles_db import run_cron_job
 
 @pytest.mark.django_db
 def test_adds_articles_to_db(mock_requests_get):
     Source.objects.create(feed_url="https://example.com/atom_feed.xml")
     Source.objects.create(feed_url="https://example.com/rss_feed.xml")
 
-    cron_return = run_cron_job()
+    call_command('daily_update_articles_db')
     retrieved_articles = Article.objects.all()
     assert retrieved_articles[0].title == "What a year building AI has taught Stack Overflow"
     assert  "Article Title 1" in retrieved_articles[0].content
