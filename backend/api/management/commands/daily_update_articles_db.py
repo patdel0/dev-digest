af4a60f69242d965dd1be9817ee3e5c16e8b1066
patdel0 <zefiru@protonmail.com>
2024-04-14 23:45:48 +0100

    chore: Improve logging and handle exceptions

--- backend/api/management/commands/daily_update_articles_db.py
+++ backend/api/management/commands/daily_update_articles_db.py
@@ -1,6 +1,10 @@
+import logging
 from django.core.management.base import BaseCommand
 from api.models import Article, Source
 from api.management.commands.curate_articles import curate_articles
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='/app/logs/manual_runs.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 class Command(BaseCommand):
     help = 'Updates articles database from sources'
@@ -10,7 +14,20 @@ class Command(BaseCommand):
         articles = curate_articles(sources)
 
         for item in articles:
-            Article.objects.create(title=item['title'], content=item['content'].get_text())
-            self.stdout.write(self.style.SUCCESS('Article added'))
+            title = item['title']
+            content = item['content'].get_text() if item['content'] is not None else ""
+
+            article, created = Article.objects.get_or_create(
+                title=title,
+                defaults={'content': content}
+            )
+
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Added: {title}'))
+                logger.info(f"{title} added to db")
+            else:
+                self.stdout.write(self.style.WARNING(f'Already exists: {title}'))
+                logger.info(f"{title} already exists in db")
 
         self.stdout.write(self.style.SUCCESS('Cron job complete'))
+        logger.info('Cron job complete')
