3ef02108feb230e7deb63f2ba87cb12604c75e18
patdel0 <zefiru@protonmail.com>
2024-04-14 12:53:01 +0100

    chore: Install django-cron to help with cron jobs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cron',
     'rest_framework',   # Django REST Framework
     'api',              # Our custom app
     'corsheaders'
