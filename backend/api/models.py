88c7c3d50075c5779251365816cf1f60737cb254
patdel0 <zefiru@protonmail.com>
2024-04-14 23:43:36 +0100

    feat: Ensure titles are unique
    Before duplicate articles were a possibility in the DB.
    Now, by adding `unique=True` a simple check is done to avoid duplicate
    articles in our dataset.

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,8 +2,8 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
     content = models.TextField()
+    title = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
