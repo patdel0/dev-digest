2f4a0b293bba17d82e7d3db073de0510cd425557
patdel0 <zefiru@protonmail.com>
2024-04-28 21:07:26 +0100

    feat: Update results `map` to use `ResultCard`

--- frontend/src/Greeting.js
+++ frontend/src/Greeting.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import SearchInputField from "./components/SearchInputField/SearchInputField";
+import ResultCard from "./components/ResultCard"
 
 function Greeting() {
   const [articles, setArticles] = useState([]);
@@ -30,18 +31,7 @@ function Greeting() {
       <h1>Articles</h1>
       <ul>
         {articles.map((article) => (
-          <li key={article.id}>
-            <h2>{article.title}</h2>
-            <p>{article.content}</p>
-            <p>Rating: {article.rating}</p>
-            <p>Excerpt: {article.excerpt}</p>
-            <p>Provider: {article.provider}</p>
-            <p>
-              Published: {new Date(article.created_at).toLocaleDateString()}
-            </p>
-            <p>Updated: {new Date(article.updated_at).toLocaleDateString()}</p>
-            <a href={article.url}>Read more</a>
-          </li>
+          <ResultCard {...article} />
         ))}
       </ul>
     </div>
